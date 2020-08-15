c80e61cdc02220ae0d42317ff165b78823c5a628
yuuya.matsueda <matsuedayuya0407@gmail.com>
2020-08-15 10:20:50 +0000

    follow, unfollow

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,30 +3,12 @@
 @section('content')
     <div class="row">
         <aside class="col-sm-4">
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title">{{ $user->name }}</h3>
-                </div>
-                <div class="card-body">
-                    {{-- ユーザのメールアドレスをもとにGravatarを取得して表示 --}}
-                    <img class="rounded img-fluid" src="{{ Gravatar::get($user->email, ['size' => 500]) }}" alt="">
-                </div>
-            </div>
+            {{-- ユーザ情報 --}}
+            @include('users.card')
         </aside>
         <div class="col-sm-8">
-            <ul class="nav nav-tabs nav-justified mb-3">
-                {{-- ユーザ詳細タブ --}}
-                <li class="nav-item">
-                    <a href="{{ route('users.show', ['user' => $user->id]) }}" class="nav-link {{ Request::routeIs('users.show') ? 'active' : '' }}">
-                        TimeLine
-                        <span class="badge badge-secondary">{{ $user->microposts_count }}</span>
-                    </a>
-                </li>
-                {{-- フォロー一覧タブ --}}
-                <li class="nav-item"><a href="#" class="nav-link">Followings</a></li>
-                {{-- フォロワー一覧タブ --}}
-                <li class="nav-item"><a href="#" class="nav-link">Followers</a></li>
-            </ul>
+            {{-- タブ --}}
+            @include('users.navtabs')
             @if (Auth::id() == $user->id)
                 {{-- 投稿フォーム --}}
                 @include('microposts.form')
@@ -36,3 +18,48 @@
         </div>
     </div>
 @endsection
+
+
+
+
+
+<!--@extends('layouts.app')-->
+
+<!--@section('content')-->
+<!--    <div class="row">-->
+<!--        <aside class="col-sm-4">-->
+<!--            <div class="card">-->
+<!--                <div class="card-header">-->
+<!--                    <h3 class="card-title">{{ $user->name }}</h3>-->
+<!--                </div>-->
+<!--                <div class="card-body">-->
+<!--                    {{-- ユーザのメールアドレスをもとにGravatarを取得して表示 --}}-->
+<!--                    <img class="rounded img-fluid" src="{{ Gravatar::get($user->email, ['size' => 500]) }}" alt="">-->
+<!--                </div>-->
+<!--            </div>-->
+<!--            {{-- フォロー／アンフォローボタン --}}-->
+<!--            @include('user_follow.follow_button')-->
+<!--        </aside>-->
+<!--        <div class="col-sm-8">-->
+<!--            <ul class="nav nav-tabs nav-justified mb-3">-->
+<!--                {{-- ユーザ詳細タブ --}}-->
+<!--                <li class="nav-item">-->
+<!--                    <a href="{{ route('users.show', ['user' => $user->id]) }}" class="nav-link {{ Request::routeIs('users.show') ? 'active' : '' }}">-->
+<!--                        TimeLine-->
+<!--                        <span class="badge badge-secondary">{{ $user->microposts_count }}</span>-->
+<!--                    </a>-->
+<!--                </li>-->
+<!--                {{-- フォロー一覧タブ --}}-->
+<!--                <li class="nav-item"><a href="#" class="nav-link">Followings</a></li>-->
+<!--                {{-- フォロワー一覧タブ --}}-->
+<!--                <li class="nav-item"><a href="#" class="nav-link">Followers</a></li>-->
+<!--            </ul>-->
+<!--            @if (Auth::id() == $user->id)-->
+<!--                {{-- 投稿フォーム --}}-->
+<!--                @include('microposts.form')-->
+<!--            @endif-->
+<!--            {{-- 投稿一覧 --}}-->
+<!--            @include('microposts.microposts')-->
+<!--        </div>-->
+<!--    </div>-->
+<!--@endsection-->
